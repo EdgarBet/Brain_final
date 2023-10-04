057c19f3db630e1c2edd4fd48eeeee49a6240286
Eduard <133205683+EdgarBet@users.noreply.github.com>
2023-10-04 18:50:44 +0300

    Add files via upload
    add full functions

--- brain_test/model/functionbrain.py
+++ brain_test/model/functionbrain.py
@@ -1,6 +1,6 @@
 from selene import Browser, by, have, be
 from selene.support.shared import browser
-import time
+from time import sleep
 from selene.support.shared.jquery_style import s, ss
 
 
@@ -10,17 +10,14 @@ class BrainFin:
 
     def open_browser(self):
         browser.open('https://brain.com.ua/ukr/')
-        return self
-
-    def maximize_window(self):
         browser.driver.maximize_window()
         return self
 
     def city_select(self, *todos: str):
         s('.mycity_container.mycity.mycityname[data-cityid="23562"]').click()
-        time.sleep(1)
+        sleep(1)
         s(by.text('Кременчук')).click()
-        time.sleep(1)
+        sleep(1)
         return self
 
     def start_page(self):
@@ -30,13 +27,18 @@ class BrainFin:
     def authorization(self):
         ss(by.text('Увійти'))[0].click()
         ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-14')
-        time.sleep(1)
+        sleep(1)
         ss('#modal-login-password-field')[0].type('769f3858b5').press_enter()
-        s('.user-panel-button.active').should(have.exact_text('Эдуард'))
+        s('.user-panel-button.active').should(have.exact_text('Едуард'))
         return self
 
     def search_text(self, checks: str):
         ss('input.quick-search-input')[1].type(checks).press_enter()
+        return self
+
+    def check_visible(self):
+        element = s('.search-products-count')
+        element.should(be.visible)
         return self
 
     def price_sorting(self):
@@ -47,18 +49,26 @@ class BrainFin:
     def verification_price(self):
         ss(by.text('Ціна: Від дешевих до дорогих'))[0].click()
         return self
-    def filter_by_price(self):
-        s('.filter').click()
+    def filter_by_producer(self):
+        ss('div.filter__title')[2].click()
         return self
 
-    def filter_check(self):
-        s('.filter').should(have.exact_text('Показати'))
+    def filter_check_note(self):
+        s('[data-filter="3-75002600000"]').click()
+        return self
+
+    def filter_check_show(self):
+        ss('.use_filter_link')[0].click()
         return self
 
     def choose_goods(self):
         s(by.text("Ноутбуки і комп'ютери")).click()
         ss(by.text('Перейти'))[0].click()
-        # s('[data-articul="82KB0006RA"]').click()
+        return self
+
+    def choose_one_good(self):
+        s('[href="/ukr/Noutbuk_Lenovo_V15_82KB0006RA-p827070.html"]').click()
+        s('[data-articul="82KB0006RA"]').click()
         return self
 
     def add_to_cart(self):
@@ -67,100 +77,49 @@ class BrainFin:
         return self
 
     def check_cart_full(self):
-        s('[data-articul="82KB0006RA"]').should(have.exact_text('1'))
+        s('.cart-button.user-cart-count.cart_custom_styles.not_empty').should(have.exact_text('1'))
         return self
 
     def delete_goods(self):
+        s('.cart-button.user-cart-count.cart_custom_styles.not_empty').click()
         ss('//button[@class="br-ci-remove"]')[1].click()
         s(by.text('Видалити не зберігаючи')).click()
         return self
 
     def check_cart_empty(self):
-        s('[data-articul="82KB0006RA"]').should(have.exact_text('Кошик порожній'))
+        s('.br-ch-block.br-cart-empty').should(have.exact_text('Ваш кошик порожній.'))
         return self
 
     def my_cabinet(self):
-        s('user-panel-button.active').click()
-        return self
-
-    def change_name(self):
-        s('#modal-user-name-field')[0].type('Едуард').press_enter()
-        return self
-
-    def verification_name(self):
-        s('user-panel-button.active').click()
+        s(by.text('Едуард')).click()
+        ss(by.text('Персональні дані'))[0].click()
+        s('input#profile-firstname-input.br-prof-form-item').clear()
+        sleep(1)
+        s('input#profile-firstname-input.br-prof-form-item').type('Едгар')
+        sleep(1)
+        s('[value="1971"]').click()
+        s('.br-prof-form-submit.update-profile-btn').click()
+        ss('//button[@class="close"][@data-dismiss="modal"]')[8].click()
         return self
 
     def logout(self):
-        s('user-panel-button.active').click()
+        s('.user-panel-button.active').click()
+        s('.logout').click()
         return self
 
     def negative_login(self):
         ss(by.text('Увійти'))[0].click()
-        ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-14').press_tab()
+        ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-')
         ss('#modal-login-password-field')[0].type('769f3858b5').press_enter()
-        s('.login-error').should(have.exact_text('Некоректний логін'))
-        time.sleep(2)
-        browser.close()
-        return self
+        s('div.login-error').should(have.exact_text('Некоректний телефон'))
+        sleep(2)
+        browser.quit()
+
 
     def negative_password(self):
         ss(by.text('Увійти'))[0].click()
-        ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-14').press_enter()
+        ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-14')
+        ss('#modal-login-password-field')[0].type('769f3858').press_enter()
         s('.login-error').should(have.exact_text('Некоректний пароль'))
-        time.sleep(2)
+        sleep(2)
         browser.close()
-        return self
-
-    def check_visible(self):
-        element = s('.search-products-count')
-        element.should(be.visible)
-        return self
-
-
-
-    # def test_brain():
-    #     # відкриваю браузер
-    #     browser.open('https://brain.com.ua/ukr/')
-    #     # роблю повний екран
-    #     browser.driver.maximize_window()
-    #     # перехожу для авторизації по кнопці Увійти
-    #     ss(by.text('Увійти'))[0].click()
-    #     time.sleep(2)
-    #     # ввожу в поле телефон номер телефону в повному форматі
-    #     ss('#modal-login-phone-field')[0].type('+38 (096) 607-20-14').press_tab()
-    #     time.sleep(2)
-    #     # ввожу в поле пароль
-    #     ss('#modal-login-password-field')[0].type('769f3858b5')
-    #     time.sleep(2)
-    #     # натискаю кнопку Увійти в авторизації
-    #     ss(by.text('Увійти'))[2].click()
-    #     time.sleep(2)
-    #     # обираю категорію товарів Ноутбуки
-    #     s(by.text("Ноутбуки і комп'ютери")).click()
-    #     time.sleep(2)
-    #     # натискаю кнопку Перейти
-    #     ss(by.text('Перейти'))[0].click()
-    #     time.sleep(2)
-    #     # обираю обраний Ноутбук
-    #     s('[href="/ukr/Noutbuk_Lenovo_V15_82KB0006RA-p827070.html"]').click()
-    #     time.sleep(2)
-    #     # захожу в сам ноутбук
-    #     s('[data-articul="82KB0006RA"]').click()
-    #     time.sleep(1)
-    #     # додаю його в кошик
-    #     s('//*[@id="br-pr-2"]/a').click()
-    #     # заходжу в кошик
-    #     ss('//button[@class="br-checkout modal-checkout"]')[0].click()
-    #     time.sleep(1)
-    #     # збільшую на 1 товар
-    #     ss('.increment')[1].click()
-    #     time.sleep(1)
-    #     # видаляю з кошика товар
-    #     ss('//button[@class="br-ci-remove"]')[1].click()
-    #     time.sleep(2)
-    #     # підтверджую видалення
-    #     s(by.text('Видалити не зберігаючи')).click()
-    #     time.sleep(2)
-    #     # закриваю браузер
-    #     browser.quit()
