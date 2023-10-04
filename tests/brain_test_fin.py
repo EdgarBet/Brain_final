b32434c9e47775eb0e5f01bd7e11ca9d3eabcd8b
Eduard <133205683+EdgarBet@users.noreply.github.com>
2023-10-04 18:50:07 +0300

    Add files via upload
    add full

--- tests/brain_test_fin.py
+++ tests/brain_test_fin.py
@@ -1,10 +1,9 @@
-import time
+from time import sleep
 
 from brain_test.model import checks
 
 
 def test_brain_e2e():
-    checks.maximize_window()
     checks.open_browser()
     checks.city_select()
     checks.start_page()
@@ -14,19 +13,27 @@ def test_brain_e2e():
     checks.start_page()
     checks.choose_goods()
     checks.price_sorting()
+    sleep(2)
     checks.verification_price()
-    # checks.filter_by_price()
-    # checks.filter_check()
-    # checks.add_to_cart()
-    # checks.check_cart_full()
-    # checks.delete_goods()
-    # checks.check_cart_empty()
-    # checks.my_cabinet()
-    # checks.change_name()
-    # checks.verification_name()
-    # checks.logout()
-    # time.sleep(2)
-    # checks.negative_login()
-    # checks.negative_password()
-
+    sleep(2)
+    checks.filter_by_producer()
+    checks.filter_check_note()
+    checks.filter_check_show()
+    sleep(2)
+    checks.choose_one_good()
+    checks.add_to_cart()
+    checks.check_cart_full()
+    sleep(2)
+    checks.delete_goods()
+    sleep(2)
+    checks.check_cart_empty()
+    checks.my_cabinet()
+    sleep(2)
+    checks.logout()
+    sleep(5)
+    checks.negative_login()
+    sleep(2)
+    checks.open_browser()
+    checks.negative_password()
+    sleep(3)
 
